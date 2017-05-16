6bde8b4d959f8dea2de76986a7b04847134c8eaa
jeremymarch <jmarch@gradcenter.cuny.edu>
2017-05-15 22:10:15 -0400

    Updated text for About and Installation activities.

--- app/src/main/java/com/philolog/hoplitekeyboard/AboutActivity.java
+++ app/src/main/java/com/philolog/hoplitekeyboard/AboutActivity.java
@@ -6,12 +6,28 @@ package com.philolog.hoplitekeyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-
+TextView tv;
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.about);
+
+    tv = (TextView) findViewById(R.id.aboutTextView);
+
+    tv.setText(Html.fromHtml("USE:<br>" +
+            "• Type the letter first, then add diacritics.<br>" +
+            "• Diacritics can be added in any order.<br>" +
+            "• Pressing the diacritic again will toggle it on or off.<br>" +
+            "• Contrasting diacritics replace each other: accents, breathings.<br>" +
+            "• Redundant diacritics replace each other: circumflex/macron." +
+            "• Press caps lock to use the diaeresis key.<br><br>" +
+            "SETTINGS:<br>" +
+            "• Precomposed will use precomposed unicode characters when possible, falling back to combining diacritics if a precomposed character doesn't exist.<br>" +
+            "• Precomposed with PUA is like the above, but will also use precomposed characters from the Private Use Area when possible.  This is only supported by certain fonts and is not standard unicode.<br>" +
+            "• Combining only uses only combining diacritics.  This is only supported by certain fonts."));
     }
 }
--- app/src/main/java/com/philolog/hoplitekeyboard/InstallationActivity.java
+++ app/src/main/java/com/philolog/hoplitekeyboard/InstallationActivity.java
@@ -2,16 +2,32 @@ package com.philolog.hoplitekeyboard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * Created by jeremy on 5/15/17.
  */
 
 public class InstallationActivity extends AppCompatActivity {
-
+    TextView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.installation);
+
+        iv = (TextView) findViewById(R.id.installationTextView);
+
+        iv.setText(Html.fromHtml("INSTALLATION<br>" +
+                "<br>" +
+                "To install keyboard on Android device:<br>" +
+                "1) Open the Settings app<br>" +
+                "2) Select Language &amp; input<br>" +
+                "3) Tap Current Keyboard<br>" +
+                "4) Tap 'Choose Keyboards'<br>" +
+                "5) Enable 'Hoplite Polytonic Greek Keyboard'<br>" +
+                "6) Now the Hoplite Keyboard can be selected from the list of keyboards<br>" +
+                "<br>" +
+                "For best results, install a polytonic Greek font such as New Athena Unicode."));
     }
 }
