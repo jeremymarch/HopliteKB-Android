b99a2d1090d29fd49207631f604c17ae3bd0ad7e
jeremymarch <jmarch@gradcenter.cuny.edu>
2017-09-02 22:18:17 -0400

    Fixes crash when adding spacing diacritic with no text before it--removed debugging comments.

--- app/src/main/java/com/philolog/hoplitekeyboard/HopliteKeyboard.java
+++ app/src/main/java/com/philolog/hoplitekeyboard/HopliteKeyboard.java
@@ -618,18 +618,17 @@ import android.view.Window;
         int maxSubstringForAccent = 7;
         String strBefore = ic.getTextBeforeCursor(maxSubstringForAccent, 0).toString();
 
-        Log.d("abc", "here0");
         int strBeforeLen = strBefore.length();
         if (strBeforeLen < 1) {
             //return;
         }
-        Log.d("abc", "here1");
+
         int cc = numCombiningChars(strBefore);
-        Log.d("abc", "here1.5");
+
         //Log.e("abc", "NUM: " + cc + ", " + (strBeforeLen - cc - 1) + ", " + (strBeforeLen) + ", " + strBefore.substring(strBeforeLen - cc - 1, strBeforeLen));
         String accentedLetter = "";
         //Log.e("abc", "unicode mode: " + unicodeMode);
-        Log.d("abc", "here2");
+
         if (strBeforeLen < 1)
         {
             accentedLetter = gv1.addAccent(acc, unicodeMode, "");
@@ -637,7 +636,7 @@ import android.view.Window;
         else {
             accentedLetter = gv1.addAccent(acc, unicodeMode, strBefore.substring(strBeforeLen - cc - 1, strBeforeLen));
         }
-        Log.d("abc", "here3");
+
         if (!accentedLetter.equals("")) {
             ic.setComposingRegion(selectionStart - (1+cc), selectionEnd);
             ic.setComposingText(accentedLetter, 1);
